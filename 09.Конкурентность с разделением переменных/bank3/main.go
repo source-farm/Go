ad864a4d127aed6828223725acb060f75b83fa32
Yunus <yunus@fastmail.com>
2018-09-11 08:02:07 +0300

    Commit

---
+++ 09.Конкурентность с разделением переменных/bank3/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "sync"
+
+var (
+	mu      sync.Mutex
+	balance int
+)
+
+func Deposit(amount int) {
+	mu.Lock()
+	balance += amount
+	mu.Unlock()
+}
+
+func Balance() int {
+	mu.Lock()
+	defer mu.Unlock()
+	return balance
+}
+
+func main() {
+}
